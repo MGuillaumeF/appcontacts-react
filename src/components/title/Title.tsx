2a2be9c5651440c0d62f4fb665b5e55d1363f262
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-19 11:41:51 +0100

    Add CSS in external files for title component

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Title.css'
 
 const Title = (props : any) => {
+    let toggle : boolean = false;
     return (
         <div>
-            <h1> Bonjour {props.nom}</h1>
+            <h1 className={toggle ? 'title-name text-error' : 'title-name text-success'}> Bonjour {props.nom}</h1>
         </div>
     );
 }
