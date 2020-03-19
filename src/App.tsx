b4ef519d5ddbb7d7873555f9cdfa3a71317e2f4f
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-19 11:55:01 +0100

    Clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Title from './components/title/Title';
 
 function App() {
-  const applicationStateName1 : any = useState('Hugo');
-  const applicationStateName2 : any = useState('Eva');
   return (
     <div className="App">
-      <Title nom={applicationStateName1[0]}/>
-      <Title nom={applicationStateName2[0]}/>
     </div>
   );
 }
