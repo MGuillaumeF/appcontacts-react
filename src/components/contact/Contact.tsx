14db3d2b9e3cc4065551548b4ae64d9c89fc2c25
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-20 04:11:22 +0100

    Add event with state change and fontawesome

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -6,18 +6,21 @@ export interface IContactProps {
     tel : string
 }*/
 
+
 export default function Contact (props: any) {
+  let [show, showContact] = React.useState(true);
   return (
-    <div className='card card-body mb-3'>
-      <h4>{props.nom}</h4>
-      <ul className='card card-body mb-3'>
+    <div className='card card-body mb-3 text-center'>
+      <h4>{props.nom}&nbsp;<i style={{cursor : 'pointer'}} className='fas fa-sort-down' onClick={() => showContact(display => !display)}></i></h4>
+      {show ? (
+        <ul className='card card-body mb-3'>
           <li className='list-group-item'>
             Email : {props.email}
           </li>
           <li className='list-group-item'>
             Téléphone : {props.tel}
           </li>
-      </ul>
+      </ul>) : null}
     </div>
   );
 }
