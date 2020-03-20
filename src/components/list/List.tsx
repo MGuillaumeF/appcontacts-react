93c6430fdbbae6772db58e65dfac4414e25368a2
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-20 03:11:24 +0100

    Add Id as key of component  list

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -8,16 +8,16 @@ export default function List (props: IListProps) {
     let state = {
             contacts : React.useState(
                 [
-                {nom : 'JonDoe', email : 'jon@gmail.fr', tel : '555-555-5555'},
-                {nom : 'JonDoe', email : 'jon@gmail.fr', tel : '555-555-5555'}
+                {id: 1, nom : 'JonDoe', email : 'jon@gmail.fr', tel : '555-555-5555'},
+                {id : 2, nom : 'JonDoe', email : 'jon@gmail.fr', tel : '555-555-5555'}
             ]
         )
     };
     console.log(state);
   return (
     <div>
-        {state.contacts[0].map((contact, index) => (
-            <Contact key={index} nom={contact.nom} email={contact.email} tel={contact.tel}/>
+        {state.contacts[0].map(contact => (
+            <Contact key={contact.id} nom={contact.nom} email={contact.email} tel={contact.tel}/>
         ))}
     </div>
   );
