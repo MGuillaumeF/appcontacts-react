71fe27e0639073939095544df5062af864f427d8
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-24 03:01:16 +0100

    Add page Error 404

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import Header from './components/header/Header'
 import ContactList from './components/contact/contact-list/ContactList';
 import { Provider } from './components/contact/contact-context/ContactContext';
 import ContactForm from './components/contact/contact-form/ContactForm';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AboutUs from './components/about-us/AboutUs';
+import Error404 from './components/error-page/Error404';
 
 function App() {
   return (
@@ -13,9 +14,12 @@ function App() {
       <Router>
         <Header/>
         <div className="container">
-          <Route exact path="/" component={ContactList}/>
-          <Route exact path="/add" component={ContactForm}/>
-          <Route exact path="/about" component={AboutUs}/>
+          <Switch>
+            <Route exact path="/" component={ContactList}/>
+            <Route exact path="/add" component={ContactForm}/>
+            <Route exact path="/about" component={AboutUs}/>
+            <Route component={Error404}/>
+          </Switch>
         </div>
 
       </Router>
---
+++ src/components/error-page/Error404.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+export default function Error404 () {
+  return (
+    <React.Fragment>
+        <h1 className='display-5 error'>Error 404</h1>
+        <p>
+            Page not found
+        </p>
+      </React.Fragment>
+  );
+}
