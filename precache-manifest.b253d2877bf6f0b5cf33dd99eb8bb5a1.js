8fedefbc787395e4c734155d04efb23ca98c061f
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-28 03:02:35 +0100

    Updates

---
+++ precache-manifest.b253d2877bf6f0b5cf33dd99eb8bb5a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113c0ba731d75587021a728ee5b00a47",
+    "url": "/appcontacts-react/index.html"
+  },
+  {
+    "revision": "fa2129f7f7b815860000",
+    "url": "/appcontacts-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5d28c6fd223cfe60523b",
+    "url": "/appcontacts-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa2129f7f7b815860000",
+    "url": "/appcontacts-react/static/js/2.bb600b5d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/appcontacts-react/static/js/2.bb600b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d28c6fd223cfe60523b",
+    "url": "/appcontacts-react/static/js/main.99abc7d7.chunk.js"
+  },
+  {
+    "revision": "9d6aad25a805a2667685",
+    "url": "/appcontacts-react/static/js/runtime-main.beda7151.js"
+  }
+]);
